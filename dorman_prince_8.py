2f695ca13b696b8f84f1dbd502924ef6ad2cc2cb
Parcifal34 <112802802+Parcifal34@users.noreply.github.com>
2024-04-14 18:07:33 +0300

    Update dorman_prince_8.py

--- dorman_prince_8.py
+++ dorman_prince_8.py
@@ -58,7 +58,7 @@ y0 = [1.0, 1.0, np.pi / 2, 7.0, 12.0, 25.0]
 t_span = [0, 3]
 
 # Решение системы уравнений
-t_eval = np.linspace(t_span[0], t_span[1], 1000)
+t_eval = np.linspace(t_span[0], t_span[1], 3000)
 sol = runge_kutta_dopri8(equations, t_span, y0, t_eval)
 
 # Извлечение решения
@@ -102,21 +102,22 @@ def animate_baton(t, X, Y, Th, l):
     ax.legend(loc='lower left')
 
     def update(frame):
-        x_cm = (m1 * 0 + m2 * l) / (m1 + m2)
-        mass1.set_data([X[frame] - x_cm * np.cos(Th[frame])], [Y[frame] - x_cm * np.sin(Th[frame])])
-        mass2.set_data([X[frame] + (l - x_cm) * np.cos(Th[frame])], [Y[frame] + (l - x_cm) * np.sin(Th[frame])])
-        line_1.set_data([X[frame] - x_cm * np.cos(Th[frame]), X[frame] + (l - x_cm) * np.cos(Th[frame])],
-                        [Y[frame] - x_cm * np.sin(Th[frame]), Y[frame] + (l - x_cm) * np.sin(Th[frame])])
+        if frame < len(t):  # Гарантируем, что индекс кадра не превышает длину массивов данных
+            x_cm = (m1 * 0 + m2 * l) / (m1 + m2)
+            mass1.set_data([X[frame] - x_cm * np.cos(Th[frame])], [Y[frame] - x_cm * np.sin(Th[frame])])
+            mass2.set_data([X[frame] + (l - x_cm) * np.cos(Th[frame])], [Y[frame] + (l - x_cm) * np.sin(Th[frame])])
+            line_1.set_data([X[frame] - x_cm * np.cos(Th[frame]), X[frame] + (l - x_cm) * np.cos(Th[frame])],
+                            [Y[frame] - x_cm * np.sin(Th[frame]), Y[frame] + (l - x_cm) * np.sin(Th[frame])])
 
-        trajectory.set_data(X[:frame + 1], Y[:frame + 1])  # Отображение траектории дубинки
-        trajectory1.set_data(X[:frame + 1] - x_cm * np.cos(Th[:frame + 1]),
-                              Y[:frame + 1] - x_cm * np.sin(Th[:frame + 1]))  # Траектория массы 1
-        trajectory2.set_data(X[:frame + 1] + (l - x_cm) * np.cos(Th[:frame + 1]),
-                              Y[:frame + 1] + (l - x_cm) * np.sin(Th[:frame + 1]))  # Траектория массы 2
+            trajectory.set_data(X[:frame + 1], Y[:frame + 1])  # Отображение траектории дубинки
+            trajectory1.set_data(X[:frame + 1] - x_cm * np.cos(Th[:frame + 1]),
+                                 Y[:frame + 1] - x_cm * np.sin(Th[:frame + 1]))  # Траектория массы 1
+            trajectory2.set_data(X[:frame + 1] + (l - x_cm) * np.cos(Th[:frame + 1]),
+                                 Y[:frame + 1] + (l - x_cm) * np.sin(Th[:frame + 1]))  # Траектория массы 2
 
         return mass1, mass2, line_1, trajectory, trajectory1, trajectory2
 
-    ani = FuncAnimation(fig, update, interval=1, blit=True)
+    ani = FuncAnimation(fig, update, interval = 1, blit=True)
     plt.show()
 
 animate_baton(t, X, Y, Th, l)
